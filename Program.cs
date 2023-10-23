922a4e0d9d39c64d12a6837f0c6f54a449ce836b
Julii Abadjiev <117214264+Jul1ius@users.noreply.github.com>
2023-10-23 17:35:10 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] inputArray = input.Split(' ');
+
+        int[] numbers = new int[inputArray.Length];
+
+        for (int i = 0; i < inputArray.Length; i++)
+        {
+            if (int.TryParse(inputArray[i], out int number))
+            {
+                numbers[i] = number;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter integers separated by spaces.");
+                return;
+            }
+        }
+        int sum = 0;
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
